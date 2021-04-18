a2974054a2ce45ba08b8c8603fe29ad7f995b432
igorzaporozhtsev <i.zaporozhtsev@spd-ukraine.com>
2021-04-18 10:35:01 +0300

    "first commit"

---
+++ src/main/kotlin/com/zeecoder/tmates/TMatesApplication.kt
@@ -0,0 +1,11 @@
+package com.zeecoder.tmates
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TMatesApplication
+
+fun main(args: Array<String>) {
+	runApplication<TMatesApplication>(*args)
+}
