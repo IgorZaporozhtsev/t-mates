433bd382864d58a3f9928042f4708cd187dedf07
igorzaporozhtsev <i.zaporozhtsev@spd-ukraine.com>
2021-04-19 08:04:53 +0300

    add to jib library

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.google.cloud.tools.jib") version "3.0.0"
 	kotlin("jvm") version "1.4.32"
 	kotlin("plugin.spring") version "1.4.32"
 	kotlin("plugin.jpa") version "1.4.32"
+}
+
+
+jib {
+	to{
+		image = "t-mates-img"
+	}
+	container {
+		ports = mutableListOf("8091")
+		format = com.google.cloud.tools.jib.api.buildplan.ImageFormat.OCI
+	}
 }
 
 group = "com.zeecoder"
