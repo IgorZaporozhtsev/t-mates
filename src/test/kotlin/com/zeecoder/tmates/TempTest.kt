fd3c334df614561900d65c4d9cfe4f0e91c1f874
igorzaporozhtsev <i.zaporozhtsev@spd-ukraine.com>
2021-04-23 16:04:54 +0300

    test build with test classes

--- src/test/kotlin/com/zeecoder/tmates/TempTest.kt
+++ src/test/kotlin/com/zeecoder/tmates/TempTest.kt
@@ -3,7 +3,7 @@ package com.zeecoder.tmates
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 
-internal class TempTest{
+class TempTest{
 
     @Test
     fun `should eq3`(){
