55cec507a71d73fe29dba3707a97808903d287c5
igorzaporozhtsev <i.zaporozhtsev@spd-ukraine.com>
2021-04-23 16:41:40 +0300

    unit test

--- src/test/kotlin/com/zeecoder/tmates/MyFirstPr.kt
+++ src/test/kotlin/com/zeecoder/tmates/MyFirstPr.kt
@@ -3,7 +3,7 @@ package com.zeecoder.tmates
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 
-class TempTest{
+class MyFirstPr{
 
     @Test
     fun `should eq3`(){
